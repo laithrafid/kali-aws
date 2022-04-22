31e6fe3f8cbf80b03bdc862592e7c511b9512099
Laith <laith@bayt.cloud>
2022-04-21 22:54:10 -0400

    commentout provider profile

--- provider.tf
+++ provider.tf
@@ -20,7 +20,7 @@ terraform {
 }
 provider "aws" {
   region     = var.aws_region
-  profile    = var.aws_profile
+  #profile    = var.aws_profile
   access_key = var.access_key
   secret_key = var.secret_key
 }
